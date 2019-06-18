209275ab0bef738b730a71d511198447d2213653
Jake <git@ja-ke.tech>
2019-06-18 12:47:43 +0200

    Add mongo test server

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const mosca = require('mosca');
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
 
 const server = new mosca.Server({port: 2222});
 
@@ -18,3 +21,20 @@ server.on('ready', setup);
 function setup() {
   console.log('Mosca server is up and running');
 } 
+
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to mongo server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
