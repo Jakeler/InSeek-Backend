754cc7b2f0f6ec61df042e864a9c5529dafc177c
Jake <git@ja-ke.tech>
2019-06-28 15:02:14 +0200

    Cleaner mqtt log

--- mqtt.ts
+++ mqtt.ts
@@ -7,12 +7,15 @@ export function init() {
         log.info('Mosca MQTT server is up and running');
     });
 
-    server.on('clientConnected', (client) => {
-        log.info('client connected ' + client.id);
-    });
+    server.on('clientConnected', (client) => log.info(`"${client.id}" connected`));
+    server.on('clientDisconnected', (client) => log.info(`"${client.id}" disconnected`));
+    
+    server.on('subscribed', (packet, client) => log.info(`"${client.id}" subscribed to ${packet}`))
 
     // fired when a message is received
     server.on('published', (packet, client) => {
-    log.info('Published ' + packet.payload);
+        if (!packet.topic.startsWith('$SYS')) { // filter out sys messages
+            log.info(`"${client.id}" published ${packet.payload} in ${packet.topic}`);
+        }
     });
 }
