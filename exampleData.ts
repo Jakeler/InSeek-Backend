088c26d9b76827b8a4e8c77e18633e1900bca8af
Jake <git@ja-ke.tech>
2019-07-02 18:19:02 +0200

    Hand picked insect images

--- exampleData.ts
+++ exampleData.ts
@@ -2,109 +2,109 @@ export const insectMetadata = [
   {
     "name": "Asiatischer Marienkäfer",
     "scientificName": "harmonia axyridis",
-    "imageUrl": "https://tse3.mm.bing.net/th?id=OIP.52yrurvf7kuZkNYnbu2B1AHaGW&pid=Api",
+    "imageUrl": "https://www.gartengnom.net/wp-content/uploads/2009/11/marienkaefer-01.jpg",
     "extract": "Der Asiatische Marienkäfer ist ein Käfer aus der Familie der Marienkäfer (Coccinellidae)."
   },
   {
     "name": "Lederwanze",
     "scientificName": "coreus marginatus",
-    "imageUrl": "https://tse3.mm.bing.net/th?id=OIP.n-UtBpgKGFA1FCY92nf89gHaE8&pid=Api",
+    "imageUrl": "https://www.lbv.de/files/user_upload/Bilder/Arten/Tiere/Insekten/Wanzen/Lederwanze-Dr-Eberhard-Pfeuffer-LBV.jpg",
     "extract": "Die Lederwanze, auch Große Randwanze oder Saumwanze genannt, gehört zur Familie der Randwanzen (Coreidae)."
   },
   {
     "name": "Dunkle Erdhummel",
     "scientificName": "bombus terrestris",
-    "imageUrl": "https://tse3.mm.bing.net/th?id=OIP.9rebeSl1rUxhFWzfBv-0PgHaE7&pid=Api",
+    "imageUrl": "https://honig-und-bienen.de/wp-content/uploads/2017/12/hummel-bombus.jpg",
     "extract": "Die Dunkle Erdhummel ist eine der in Europa am häufigsten vorkommenden und auch größten Arten der Hummeln. Sie ist ein schwarzes Tier mit zwei gelben Querbinden und einer weißen Hinterleibspitze. Die begatteten Jungköniginnen der Dunklen Erdhummeln zählen zu den frühesten, die in einem Kalenderjahr beobachtet werden können. Sie sind in der Regel bereits im Februar und März dabei zu beobachten, wie sie nach einer geeigneten Stelle für die Anlage des Hummelnestes suchen. Die Färbung der Dunklen Erdhummeln ähnelt derjenigen der Hellgelben Erdhummel so stark, dass sie in der freien Natur kaum unterschieden werden können. Schmarotzer der Dunklen Erdhummel ist die Keusche Kuckuckshummel."
   },
   {
     "name": "Gemeine Feuerwanze",
     "scientificName": "pyrrhocoris apterus",
-    "imageUrl": "https://tse4.mm.bing.net/th?id=OIP.-W1435r_Ri9XGJwOlOKMxQHaHW&pid=Api",
+    "imageUrl": "https://www.tierfuehrer.de/media/image/88/44/e4/gemeine-feuerwanze-01_2500x2500.jpg",
     "extract": "Die Gemeine Feuerwanze ist eine Art aus der Familie der Feuerwanzen (Pyrrhocoridae). Die durch ihre Färbung sehr auffallende Art wird meist einfach Feuerwanze genannt, sie wird im Volksmund aber auch als Schuster- oder Feuerkäfer bezeichnet, also irrtümlich für einen Käfer gehalten. Die Tiere leben gesellig, so dass man sie in den von ihnen bevorzugten Biotopen häufig in großer Zahl findet."
   },
   {
     "name": "Gerippter Brachkäfer",
     "scientificName": "amphimallon solstitiale",
-    "imageUrl": "https://tse2.mm.bing.net/th?id=OIP.2aadWZGQgT9URJfUNFw00wHaEj&pid=Api",
+    "imageUrl": "https://dyaaf063c1cms.cloudfront.net/b4a39143fea0aa831399547fa5841c8c/large_Gerippter%20Brachkäfer1.jpg?v=1501267461",
     "extract": "Der Gerippte Brachkäfer ist ein Käfer aus der Familie der Blatthornkäfer (Scarabaeidae)."
   },
   {
     "name": "Borkenkäfer",
     "scientificName": "scolytinae",
-    "imageUrl": "https://tse4.mm.bing.net/th?id=OIP.OENdUHMnFNQUYfeHEJGjXQHaDy&pid=Api",
+    "imageUrl": "https://bnn.de/wp-content/uploads/2019/01/112021535-e1546877776309-1200x857.jpeg",
     "extract": "Die Borkenkäfer (Scolytinae) sind eine Unterfamilie der Rüsselkäfer (Curculionidae)."
   },
   {
     "name": "Gemeine Waldschabe",
     "scientificName": "ectobius lapponicus",
-    "imageUrl": "https://tse2.mm.bing.net/th?id=OIP.7crxzVyQKJKXTylF-1KcPAHaFj&pid=Api",
+    "imageUrl": "http://tierdoku.de/images/Lapplaendische-Waldschabe.jpg",
     "extract": "Die Gemeine Waldschabe oder Lappland-Waldschabe ist eine europäische Art der Waldschaben. Sie lebt im Freiland und kommt nur selten und ausnahmsweise in Gebäuden vor."
   },
   {
     "name": "Grünes Heupferd",
     "scientificName": "tettigonia viridissima",
-    "imageUrl": "https://tse2.mm.bing.net/th?id=OIP.Iz_EX_0cdeh7xWKa6bzcPQHaE7&pid=Api",
+    "imageUrl": "http://www.naturfotografie-radloff.de/bilder/gruenes-heupferd-tettigonia-viridissima-2.jpg",
     "extract": "Das Grüne Heupferd, auch Großes Heupferd, Großes Grünes Heupferd, selten auch Grüne Laubheuschrecke genannt, ist eine der größten in Mitteleuropa vorkommenden Langfühlerschrecken aus der Überfamilie der Laubheuschrecken (Tettigonioidea). Die Art gehört zu den häufigsten Laubheuschrecken Mitteleuropas."
   },
   {
     "name": "Feldgrille",
     "scientificName": "gryllus campestris",
-    "imageUrl": "https://tse2.mm.bing.net/th?id=OIP.xWQuKcEVzPVNawKIGBhoUgHaF4&pid=Api",
+    "imageUrl": "https://www.umweltnetz-schweiz.ch/media/k2/items/cache/ae4bbf432d7a347a7d7b6f051b071f94_XL.jpg",
     "extract": "Die Feldgrille ist eine Art aus der Familie der Echten Grillen (Gryllidae) innerhalb der Ordnung der Heuschrecken (Orthoptera). Sie war 2003 das Insekt des Jahres in Deutschland, und 2014 das Tier des Jahres in der Schweiz."
   },
   {
     "name": "Gemeine Blutzikade",
     "scientificName": "cercopis vulnerata",
-    "imageUrl": "https://tse1.mm.bing.net/th?id=OIP.e1X2tU78wp6USGAPVgISNQHaFV&pid=Api",
+    "imageUrl": "https://naturschutz.ruhr/wp-content/uploads/2018/05/Blutzikade_MMB1798.jpg",
     "extract": "Blutzikaden (Cercopidae), engl. froghoppers, sind eine Familie der Rundkopfzikaden (Cicadomorpha) aus der Überfamilie der Cercopoidea. Diese Insekten sind auffällig schwarz-rot gezeichnet – daher der Name –, im Gegensatz zu den bräunlich und strohfarbenen Schaumzikaden (Aphrophoridae), ihrer Schwestergruppe. Ein weiteres Unterscheidungsmerkmal ist, dass die Larven von Schaumzikaden in selbst erzeugten Schaumhüllen leben, dem sogenannten Kuckucksspeichel."
   },
   {
     "name": "Admiral",
     "scientificName": "vanessa atalanta",
-    "imageUrl": "https://tse3.mm.bing.net/th?id=OIP.wHULYAH1VftVQ1H31T5h6AHaE9&pid=Api",
+    "imageUrl": "http://www.schwalbenschwanz.ch/fotos/Admiral/JuergenSchmidt_Admiral.jpg",
     "extract": "Der Admiral ist ein sehr bekannter und weit verbreiteter Schmetterling der nördlichen Hemisphäre aus der Familie der Edelfalter (Nymphalidae). Das Artepitheton leitet sich von Atalanta, einer Jägerin aus der griechischen Mythologie ab."
   },
   {
     "name": "Grüne Stinkwanze",
     "scientificName": "palomena prasina",
-    "imageUrl": "https://tse3.mm.bing.net/th?id=OIP.QOfqzqEC8nYmdEyqKpUZOQHaFj&pid=Api",
+    "imageUrl": "http://www.naturkamera.de/images/insekten/wanzen/stinkwanze-gruen/stinkwanze-180503-nk.jpg",
     "extract": "Die Grüne Stinkwanze, auch als Gemeine Stinkwanze oder Gemeiner Grünling bezeichnet, ist eine Wanze aus der Familie der Baumwanzen (Pentatomidae)."
   },
   {
     "name": "Hainschwebfliege",
     "scientificName": "episyrphus balteatus",
-    "imageUrl": "https://tse2.mm.bing.net/th?id=OIP.q4LoxIoCr-3gYQnsR_QJrwHaFT&pid=Api",
+    "imageUrl": "https://img.fotocommunity.com/hainschwebfliege-a5497f75-9527-416c-9685-3963eb19f669.jpg?height=1080",
     "extract": "Die Hainschwebfliege, auch bekannt als Wander- oder Winterschwebfliege, ist eine Art der Familie der Schwebfliegen (Syrphidae). 2004 wurde sie zum Insekt des Jahres in Deutschland gewählt."
   },
   {
     "name": "Tagpfauenauge",
     "scientificName": "aglais io",
-    "imageUrl": "https://tse3.mm.bing.net/th?id=OIP.MYtCWcUss28XZjCPGRMLpQHaE8&pid=Api",
+    "imageUrl": "https://media04.lokalkompass.de/article/2019/03/01/1/10028781_L.jpg?1551599951",
     "extract": "Das Tagpfauenauge ist ein Schmetterling (Tagfalter) aus der Familie der Edelfalter (Nymphalidae). Das Artepitheton leitet sich von Io, einer Geliebten des Zeus aus der griechischen Mythologie ab. Das Tagpfauenauge wurde zum Schmetterling des Jahres 2009 gewählt."
   },
   {
     "name": "Gemeine Florfliege",
     "scientificName": "chrysoperla carnea",
-    "imageUrl": "https://tse2.mm.bing.net/th?id=OIP.I1_GlpU35m0o4l1x3HX-8AHaEn&pid=Api",
+    "imageUrl": "https://www.naturspaziergang.de/Andere/Andere-Fotos/Chrysoperla_carnea_02_08-06-09.jpg",
     "extract": "Die Gemeine Florfliege oder Grüne Florfliege ist ein Netzflügler der Familie der Florfliegen (Chrysopidae). Sie wurde 1999 in Deutschland zum Insekt des Jahres gewählt."
   },
   {
     "name": "Saddleback caterpillar",
     "scientificName": "acharia stimulea",
-    "imageUrl": "https://tse1.mm.bing.net/th?id=OIP.vUAjJ-BKE5rIotDJO2GZ9gHaE8&pid=Api",
+    "imageUrl": "http://static1.squarespace.com/static/502d2cede4b0ab396711e089/t/55eaf71ce4b07f8c77913dbb/1441462047578/?format=1500w",
     "extract": "Acharia stimulea ist ein in Nordamerika vorkommender Schmetterling (Nachtfalter) aus der Familie der Schneckenspinner (Limacodidae)."
   },
   {
     "name": "Giraffenhalskäfer",
     "scientificName": "trachelophorus giraffa",
-    "imageUrl": "https://tse4.mm.bing.net/th?id=OIP.4CU3mOTrBMRfXQZ813zIMQHaFd&pid=Api",
+    "imageUrl": "https://img.fotocommunity.com/giraffenhalskaefer-trachelophorus-giraffa-madagaskar-ae10d1c4-6643-4f10-8035-39125b0c0521.jpg?width=1000",
     "extract": "Der Giraffenhalskäfer ist ein Käfer aus der Familie der Blattroller (Attelabidae). Sein Name ist von seinem auffallend langen Hals abgeleitet."
   },
   {
     "name": "Gemeiner Steinläufer",
     "scientificName": "lithobius forficatus",
-    "imageUrl": "https://tse4.mm.bing.net/th?id=OIP.wje9hRekcXE8hyPwEhc4igHaE6&pid=Api",
+    "imageUrl": "https://www.fkohl.de/photos/Lithobius-forficatus/27.12.06-46095_1024.jpg",
     "extract": "Der Gemeine Steinläufer ist eine in Mitteleuropa weit verbreitete Art aus der Ordnung der Steinläufer (Lithobiomorpha)."
   }
 ];
@@ -123,7 +123,7 @@ export const genCupList = (suitcaseID: string) => [
   }, 
   ...(Array.apply(null, {length: 10})).map(() => (
     {
-      ip: "0.0.0.0",
+      ip: "0.0.0.1",
       syncStatus: "pending",
     }))
 ];
